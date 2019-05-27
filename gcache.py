9fc4b2048fab61d8ae696c4a2f9cdddca1b75431
Ronald Chan <ronaldchanchl0@gmail.com>
2019-05-27 17:29:06 +0800

    Cache now tracks the last retrieval time for each entry

--- gcache.py
+++ gcache.py
@@ -16,6 +16,7 @@
 
 import json
 import zlib
+import time
 import sqlite3
 import threading
 from input_methods.gcantonese.gtypes import *
@@ -37,7 +38,8 @@ class GCacheService:
                     suggestions BLOB NOT NULL,
                     cached_pages INTEGER NOT NULL,
                     max_pages INTEGER NOT NULL,
-                    requested_time REAL NOT NULL
+                    requested_time REAL NOT NULL,
+                    last_retrieved REAL NOT NULL
                 );
                 CREATE UNIQUE INDEX IF NOT EXISTS request_idx ON requests(request);
             """)
@@ -56,8 +58,8 @@ class GCacheService:
                                    grequest.suggestions))
                 conn.execute("""
                     INSERT OR REPLACE INTO requests(request, suggestions, cached_pages, 
-                                                    max_pages, requested_time)
-                    VALUES(?,?,?,?,?)
+                                                    max_pages, requested_time, last_retrieved)
+                    VALUES(?,?,?,?,?,?)
                 """, (
                         grequest.request,
                         sqlite3.Binary(
@@ -69,6 +71,7 @@ class GCacheService:
                         grequest.requested_pages,
                         grequest.max_pages,
                         grequest.requested_time,
+                        grequest.requested_time
                      )
                 )
                 conn.commit()
@@ -77,18 +80,23 @@ class GCacheService:
         with self.threadlock:
             with sqlite3.connect(self.cache_path) as conn:
                 cur = conn.execute("""
-                    SELECT suggestions, cached_pages, max_pages, requested_time
+                    SELECT id, suggestions, cached_pages, max_pages, requested_time
                     FROM requests
                     WHERE request = ?;
                 """, (query,))
                 row = cur.fetchone()
                 if row == None:
                     return None
+                id = row[0]
                 result = GRequest()
                 result.request = query
                 result.suggestions = list(map(lambda x: GSuggestion(x[0],x[1],x[2]), 
-                                          json.loads(zlib.decompress(row[0]).decode('utf-8'))))
-                result.requested_pages = row[1]
-                result.max_pages = row[2]
-                result.requested_time = row[3]
+                                          json.loads(zlib.decompress(row[1]).decode('utf-8'))))
+                result.requested_pages = row[2]
+                result.max_pages = row[3]
+                result.requested_time = row[4]
+                conn.execute("""
+                    UPDATE requests SET last_retrieved = ? WHERE id = ?
+                """, (time.time(), id))
+                conn.commit()
                 return result

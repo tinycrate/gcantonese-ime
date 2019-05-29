ced8fac350e335806c895a21fca740bd46d558b1
Ronald Chan <ronaldchanchl0@gmail.com>
2019-05-29 17:31:06 +0800

    Clean up for release v0.1

--- gcantonese/gretrieve.py
+++ gcantonese/gretrieve.py
@@ -1,18 +1,4 @@
 #! python3
-#
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 2.1 of the License, or (at your option) any later version.
-#
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with this library; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 import os
 import re
@@ -120,7 +106,7 @@ class GWordRetrievalService:
                 if cached.requested_time > grequest.requested_time:
                     print("[{}, {}]: Skipping save, cache is newer!".format(ascii(input_str), pages))
                     self.requesting.pop(input_str, 0)
-                    return            
+                    return
             self.cache.put(grequest)
             self.requesting.pop(input_str, 0)
         print("[{}, {}]: Request completed!".format(ascii(input_str), pages))
